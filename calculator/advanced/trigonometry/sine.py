e81123aa600d23c30b724388d13363e20e29db96
holaamigoo <ekey.ju@gmail.com>
2024-11-13 17:07:29 +0300

    fixed

--- calculator/advanced/trigonometry/sine.py
+++ calculator/advanced/trigonometry/sine.py
@@ -1,9 +1,12 @@
 import math
 from calculator.advanced.root import square_root
+
+
 # from ..root import square_root НЕ РАБОТАЕТ
 
 
 def sin(x):
     return math.sin(x)
+
 
 print(square_root(4))
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from calculator import *
 #from calculator import basic
 import sys
 print(sys.path)
-print(basic.subtraction.add(2, 3))         # Вывод: 5 (указать путь при import *)
+print(basic.addition.add(2, 3))         # Вывод: 5 (указать путь при import *)
 print(subtract(5, 2))        # Вывод: 3
 print(power(2, 3))           # Вывод: 8
 print(square_root(16))       # Вывод: 4.0
